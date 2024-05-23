f7fe813db197d8f6b8abc324b87c5b724bd9af8c
netocalegari <netokalegari@gmail.com>
2024-05-23 11:17:09 -0300

    SectionList

--- Lists/App.js
+++ Lists/App.js
@@ -6,15 +6,32 @@ import {
   Text,
   View,
   FlatList,
+  SectionList,
 } from "react-native";
 import pokemonList from "./data.json";
+import groupedPokemonList from "./grouped-data.json";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <StatusBar backgroundColor="black" /> */}
       <View style={styles.listView}>
-        <FlatList
+        <SectionList
+          sections={groupedPokemonList}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.card}>
+                <Text style={styles.cardText}>{item}</Text>
+              </View>
+            );
+          }}
+          renderSectionHeader={({ section }) => (
+            <Text style={styles.sectionHeaderText}>{section.type}</Text>
+          )}
+          ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+          SectionSeparatorComponent={() => <View style={{ height: 16 }} />}
+        ></SectionList>
+        {/* <FlatList
           data={pokemonList}
           renderItem={({ item }) => {
             return (
@@ -25,8 +42,16 @@ export default function App() {
             );
           }}
           keyExtractor={(item, index) => item.id.toString()}
-          // horizontal
-        />
+          ItemSeparatorComponent={<View style={{ height: 16 }} />}
+          ListEmptyComponent={<Text>No items found.</Text>}
+          ListHeaderComponent={
+            <Text style={styles.headerText}>Pokémon List</Text>
+          }
+          ListFooterComponent={
+            <Text style={styles.footerText}>End of List</Text>
+          }
+          // horizontal={true} // sets the list as horizontal
+        /> */}
       </View>
     </View>
   );
@@ -46,9 +71,26 @@ const styles = StyleSheet.create({
     padding: 16,
     borderRadius: 8,
     borderWidth: 1,
-    marginBottom: 16,
+    // marginBottom: 16,
   },
   cardText: {
     fontSize: 30,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  footerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 12,
+  },
+  sectionHeaderText: {
+    // backgroundColor: "white",
+    fontSize: 24,
+    fontWeight: "bold",
   },
 });
