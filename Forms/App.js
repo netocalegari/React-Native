76ab5a2cd0c02c55f2137cf526d3e9a8fbbea069
netocalegari <netokalegari@gmail.com>
2024-05-24 10:31:43 -0300

    Switch

--- Forms/App.js
+++ Forms/App.js
@@ -5,15 +5,40 @@ import {
   StatusBar,
   SafeAreaView,
   TextInput,
+  Switch,
 } from "react-native";
 import { useState } from "react";
 
 export default function App() {
   const [name, setName] = useState("");
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
     <SafeAreaView style={styles.container}>
-      <TextInput style={styles.input} value={name} onChangeText={setName} />
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="email@example.com"
+        // autoCorrect={true}
+        // autoCapitalize="characters"
+        // keyboardType=""
+        // secureTextEntry={true}
+      />
+      <TextInput
+        style={[styles.input, styles.multilineText]}
+        placeholder="Message"
+        multiline={true}
+      />
       <Text style={styles.text}>My name is {name}</Text>
+      <View style={styles.switchContainer}>
+        <Text style={styles.text}>Dark mode</Text>
+        <Switch
+          value={isDarkMode}
+          onValueChange={() => setIsDarkMode((previousState) => !previousState)}
+          trackColor={{ false: "#767577", true: "lightblue" }}
+          thumbColor={"#F4F3F4"}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -33,5 +58,15 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 30,
     padding: 10,
+  },
+  multilineText: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
   },
 });
