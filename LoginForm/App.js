3eb139cce1bbcab767c42e1512206a449458131c
netocalegari <netokalegari@gmail.com>
2024-05-24 10:36:13 -0300

    first commit

---
+++ LoginForm/App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
