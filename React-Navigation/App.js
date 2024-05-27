ad3ec3217781adb129b32fb35c3ed58554eb6a8d
netocalegari <netokalegari@gmail.com>
2024-05-27 11:33:55 -0300

    first commit

---
+++ React-Navigation/App.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+
+export default function App() {
+  return <View style={styles.container}></View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
