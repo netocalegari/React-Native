ec2370a3340653fff75029e2b1e2d18bc121665a
netocalegari <netokalegari@gmail.com>
2024-06-06 08:35:02 -0300

    Button component

--- Rocketseat/IgniteTeams/src/screens/Groups/index.tsx
+++ Rocketseat/IgniteTeams/src/screens/Groups/index.tsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+
 import Header from "@components/Header";
 import { Container } from "./styles";
 import Highlight from "@components/Highlight";
 import GroupCard from "@components/GroupCard";
+import { FlatList } from "react-native";
+import ListEmpty from "@components/ListEmpty";
+import Button from "@components/Button";
+
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>([
+    // "Chonk Bois",
+    // "BigChungusses",
+    // "Evil Bunnies",
+  ]);
   return (
     <Container>
       <Header />
       <Highlight title="Teams" subTitle="Play with you team" />
-      <GroupCard title="Chonk Bois" />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => <ListEmpty message="No groups found" />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+      />
+      <Button title="Add Team" />
     </Container>
   );
 }
